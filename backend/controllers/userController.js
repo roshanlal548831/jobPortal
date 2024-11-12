f510d84c5ac1bb5d9dc480e47deca4aa802bef2f
roshanlal548831 <roshanlal160405@gmail.com>
2024-11-12 20:35:58 +0530

    something chaned

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,5 @@
 import { User } from "../model/userModel.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import getDataUri from "../utils/dataUri.js";
 import cloudinary from "../utils/cloudinary.js";
