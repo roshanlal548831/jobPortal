3152bf500105d0351db8bf997e4d41698a23699e
roshanlal548831 <roshanlal160405@gmail.com>
2024-11-12 20:40:51 +0530

    Auth

--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -4,7 +4,7 @@ import { getAdminJobs, getAllJob, getJobById, postJob } from "../controllers/job
 const router = express.Router();
 
 router.route("/post").post(isAuthenticated,postJob)
-router.route("/get").get(isAuthenticated,getAllJob)
+router.route("/get").get(getAllJob)
 router.route("/get/adminjob").get(isAuthenticated,getAdminJobs)
 router.route("/get/:id").get(isAuthenticated,getJobById)
 
